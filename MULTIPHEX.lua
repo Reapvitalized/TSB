a3332610a5940cc23afd5ea21631c4689e122a97
Reapvitalized <realvitalization@proton.me>
2025-04-30 08:55:00 +0800

    Update MULTIPHEX.lua

--- MULTIPHEX.lua
+++ MULTIPHEX.lua
@@ -1,3 +1,58 @@
+
+function info(txt,dly)
+if dly == nil then dly = 2 end
+spawn(function() 
+local ScreenGui = Instance.new("ScreenGui")
+local MainFrame = Instance.new("Frame")
+local Credits = Instance.new("TextLabel")
+pcall(function() game.CoreGui.Revit:Destroy() end)
+task.wait(.1)
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
+ScreenGui.IgnoreGuiInset = true
+ScreenGui.ResetOnSpawn = false
+ScreenGui.Name = 'Revit'
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = ScreenGui
+MainFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+MainFrame.BorderColor3 = Color3.fromRGB(255, 0, 0)
+
+
+spawn(function()
+Credits.Parent = ScreenGui
+Credits.Font=Enum.Font.Arcade
+Credits.TextColor3=Color3.new(1,1,1)
+Credits.Position = UDim2.new(0,0,0,0)
+Credits.TextSize = 35
+Credits.Size = UDim2.new(1,0,.1,0)
+Credits.Text = ''
+Credits.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Credits.BorderColor3 = Color3.fromRGB(255, 0, 0)
+end)
+function tw(var,s)
+local a = ""
+local s_l = #s
+for i = 1, s_l do
+local c = string.sub(s, i, i)
+a = a .. c
+var.Text = a
+if c == "." then
+task.wait(.6)
+elseif c == ";" then
+task.wait(.3)
+elseif c == "," then
+task.wait(.3)
+elseif c == "!" then
+task.wait(.3)
+end
+task.wait(.03)
+end
+end
+tw(Credits,txt)
+task.wait(dly)
+ScreenGui:Destroy()
+end)
+end
 function GetAudio(audioname)
 local result
 local success, response = pcall(function()
