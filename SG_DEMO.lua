4fb9032deb1bb277a8940c955d9b3423971e5625
Reapvitalized <realvitalization@proton.me>
2025-05-27 08:48:46 +0800

    Update SG_DEMO.lua
    woops

--- SG_DEMO.lua
+++ SG_DEMO.lua
@@ -20081,7 +20081,7 @@ s.PlaybackSpeed = 1
 s.Volume = 4 s:Play()
 task.delay(1,function()
 s:Destroy() end)
-setclipboard('Kyra#9760')
+setclipboard('reap4985')
 tw(cb,'Copied!',.05)
 cb.TextColor3=Color3.new(0, 1, 0)
 cb.BorderColor3=Color3.new(0, 1, 0)
@@ -20129,59 +20129,9 @@ Credits.Text = ''
 Credits.BackgroundTransparency = 1
 MainFrame.Text = 
 [[
-> > You are not allowed to:
-> > Distribute the script
-> > Reverse Engineer, Modify the code (Settings unincluded)
-
-> > You are allowed to:
-> > Modify the settings given from each script
-
-
-
-**SOFTWARE LICENSE AGREEMENT**
-
-This Software License Agreement ("Agreement") is entered into on 12/16/24 ("Effective Date")
-
-**1. DEFINITIONS**
-
-* "script" means the closed source and described and meant to be as "Early Access" paid scripts distributed by either the current original owner(s) of the several GitHub accounts listed here.
-https://github.com/Cv-jsSBzZ-U825F-ViuMPA-xo
-https://github.com/Reapvitalized,
-including all updates, modifications, and enhancements of the script.
-* "Licensee" means the individual or entity using the Software.
-
-**2. GRANT OF LICENSE**
-
-Licensor grants to Licensee a non-exclusive, non-transferable license to use the Software solely for personal, commercial, etc..
-The license can be modified at any time.
-
-**3. RESTRICTIONS**
-
-* Licensee shall not modify, reverse engineer, or disassemble the script.
-* Licensee shall not distribute, sublicense, or assign the script to any third party.
-* Licensee shall not use the script for any purpose other than as specified in Section 2.
-
-**4. OWNERSHIP**
-
-The script and all intellectual property rights thereto are and shall remain the exclusive property of Licensor.
-
-**5. TERM AND TERMINATION**
-
-This Agreement shall commence on the Effective Date and shall continue until terminated by Licensor. Licensor may terminate this Agreement at any time, without notice, if Licensee breaches any term or condition of this Agreement.
-
-**6. WARRANTY DISCLAIMER**
-
-The script is provided "as is" without warranty of any kind, express or implied.
-
-**7. LIMITATION OF LIABILITY**
-
-In no event shall Licensor be liable for any damages, including but not limited to incidental, consequential, or punitive damages, arising out of the use of the script.
-
-**8. ENTIRE AGREEMENT**
-
-This Agreement constitutes the entire agreement between the parties and supersedes all prior or contemporaneous agreements or understandings.
-
-By using this script, Licensee acknowledges that it has read, understands, and agrees to be bound by the terms and conditions of this Agreement.
+have fun bro
+top right press credits or liscense to remove this ui
+fuck golden 
 ]]
 License.Parent = ScreenGui
 License.Font=Enum.Font.Arcade
